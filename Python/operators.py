c8862d755efc75286ebf5b77aa8e5615f65ab448
Ronish J Gowda <129502957+ronish21@users.noreply.github.com>
2023-04-18 14:47:08 +0530

    Add files via upload

---
+++ Python/operators.py
@@ -0,0 +1,63 @@
+
+# Arithmetic Operators
+x = 10
+y = 5
+print(x + y)  # Addition operator
+print(x - y)  # Subtraction operator
+print(x * y)  # Multiplication operator
+print(x / y)  # Division operator
+print(x % y)  # Modulo operator
+print(x ** y) # Exponentiation operator
+
+# Comparison Operators
+x = 10
+y = 5
+print(x > y)   # Greater than operator
+print(x >= y)  # Greater than or equal to operator
+print(x < y)   # Less than operator
+print(x <= y)  # Less than or equal to operator
+print(x == y)  # Equal to operator
+print(x != y)  # Not equal to operator
+
+# Logical Operators
+x = True
+y = False
+print(x and y) # Logical AND operator
+print(x or y)  # Logical OR operator
+print(not x)   # Logical NOT operator
+
+# Bitwise Operators
+x = 10
+y = 5
+print(x & y)   # Bitwise AND operator
+print(x | y)   # Bitwise OR operator
+print(x ^ y)   # Bitwise XOR operator
+print(~x)      # Bitwise NOT operator
+print(x << 2)  # Bitwise left shift operator
+print(x >> 2)  # Bitwise right shift operator
+
+# Assignment Operators
+x = 10
+x += 5  # Addition assignment operator
+print(x)
+x -= 5  # Subtraction assignment operator
+print(x)
+x *= 2  # Multiplication assignment operator
+print(x)
+x /= 2  # Division assignment operator
+print(x)
+x %= 3  # Modulo assignment operator
+print(x)
+x **= 2 # Exponentiation assignment operator
+print(x)
+
+# Membership Operators
+x = [1, 2, 3]
+print(1 in x)  # Check if 1 is in the list
+print(4 not in x)  # Check if 4 is not in the list
+
+# Identity Operators
+x = 10
+y = 10
+print(x is y)  # Check if x and y refer to the same object
+print(x is not y)  # Check if x and y do not refer to the same object
